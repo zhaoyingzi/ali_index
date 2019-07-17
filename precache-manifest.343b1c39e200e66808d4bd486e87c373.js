a1a8f368ac6d5e73da3caf0479a0b9afc4c33702
unknown <zhaoyingzi>
2019-07-18 00:20:15 +0800

    Updates

---
+++ precache-manifest.343b1c39e200e66808d4bd486e87c373.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee6243c95213da5c9fcb18c4620b474",
+    "url": "/ali_index/index.html"
+  },
+  {
+    "revision": "773063c4c13b9915f7f2",
+    "url": "/ali_index/static/css/main.3ed62dc3.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/ali_index/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "773063c4c13b9915f7f2",
+    "url": "/ali_index/static/js/main.f8744161.chunk.js"
+  },
+  {
+    "revision": "c89b453261a9bfbeaa7e",
+    "url": "/ali_index/static/js/runtime~main.60a51664.js"
+  }
+]);
